f16d6f823494eced4d880d3e12755aa85a838e31
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2022-08-26 10:42:45 +0200

    Add lokal state and line-through css

--- src/components/ShoppingListItem.js
+++ src/components/ShoppingListItem.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./ShoppingListItem.css";
 
 export default function ShoppingListItem({ name, onDelete }) {
+  const [isDone, setIsDone] = useState(false);
+  const className = isDone ? "ListItem__strike-through" : "";
+
   return (
     <li>
-      <input type="checkbox" id={name} />
-      <label htmlFor={name}>{name}</label>
+      <input onChange={() => setIsDone(!isDone)} type="checkbox" id={name} />
+      <label htmlFor={name} className={className}>
+        {name}
+      </label>
       <button
         className="ListItem__button"
         onClick={onDelete}
