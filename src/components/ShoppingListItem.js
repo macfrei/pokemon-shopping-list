f85c90fe83f8d8175e482a1eaa68b007f11dd3ff
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2022-08-26 10:29:50 +0200

    Add delete button to shopping list item

--- src/components/ShoppingListItem.js
+++ src/components/ShoppingListItem.js
@@ -1,8 +1,9 @@
-export default function ShoppingListItem({ name }) {
+export default function ShoppingListItem({ name, onDelete }) {
   return (
     <li>
       <input type="checkbox" id={name} />
       <label htmlFor={name}>{name}</label>
+      <button onClick={onDelete}>Delete Item</button>
     </li>
   );
 }
